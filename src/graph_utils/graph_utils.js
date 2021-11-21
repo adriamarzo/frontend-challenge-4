9fb2b2a57fe3b4cdaf6715c18c3fe975b859a9be
adria marzo <adriamarzo@gmail.com>
2021-11-21 10:42:18 +0100

    feat(graph_utils): implement getNodeInputConnections

--- src/graph_utils/graph_utils.js
+++ src/graph_utils/graph_utils.js
@@ -3,65 +3,81 @@
  */
 
 /**
+ * getObjectValues: Get an array with the values of an object.
+ *
+ * @param {Object} object generic object.
+ * @returns {Array} Array with attribute values.
+ */
+const getObjectValues = (object) => (object ? Object.values(object) : []);
+
+/**
+ * getConnectionsModel: Get the model of the connections.
+ *
+ * @param {Object} connections Object with all the connections.
+ * @returns {Array} Array of connections.
+ */
+const getConnectionsModel = ({ connections }) => getObjectValues(connections);
+
+/**
+ * getNodesModel: Get the model of the nodes.
+ *
+ * @param {Object} nodes Object with all the nodes.
+ * @returns {Array} Array of nodes.
+ */
+const getNodesModel = ({ nodes }) => getObjectValues(nodes);
+
+/**
  * getNodeInputConnections: Get a node input connections.
- * 
+ *
  * @param {Object} node Node model object.
  * @param {Object} graph Bot model object.
  * @returns {Array} Array of Connection models.
  */
 export function getNodeInputConnections(node, graph) {
-  
+  return getConnectionsModel(graph).filter(
+    ({ targetPath }) => targetPath === node.id
+  );
 }
 
 /**
  * getNodeOutputConnections: Get a node output connections.
- * 
+ *
  * @param {Object} node Node model object.
  * @param {Object} graph Bot model object.
  * @returns {Array} Array of Connection models.
  */
-export function getNodeOutputConnections(node, graph) {
-  
-}
+export function getNodeOutputConnections(node, graph) {}
 
 /**
  * getNodeConnections: Get a node connections.
- * 
+ *
  * @param {Object} node Node model object.
  * @param {Object} graph Bot model object.
  * @returns {Array} Array of Connection models.
  */
-export function getNodeConnections(node, graph) {
-  
-}
+export function getNodeConnections(node, graph) {}
 
 /**
  * getLeafNodes: Get all leaf nodes (no output connections).
- * 
+ *
  * @param {Object} graph Bot model object.
  * @returns {Array} Array of Node models.
  */
-export function getLeafNodes(graph) {
-  
-}
+export function getLeafNodes(graph) {}
 
 /**
  * getRootNodes: Get all root nodes (no input connections).
- * 
+ *
  * @param {Object} graph Bot model object.
  * @returns {Array} Array of Node models.
  */
-export function getRootNodes(graph) {
-  
-}
+export function getRootNodes(graph) {}
 
 /**
  * hasMultipleSources: Determines if a node is reachable from different sources.
- * 
+ *
  * @param {Object} node Node model object.
  * @param {Object} graph Bot model object.
  * @returns {Boolean} True if a node has different source paths.
  */
-export function hasMultipleSources(node, graph) {
-  
-}
+export function hasMultipleSources(node, graph) {}
