552546c9ead7a1e89f899b46551ac2ea90e48344
adria marzo <adriamarzo@gmail.com>
2021-11-22 10:39:32 +0100

    refactor(doc): add jsdoc to utils and models

--- src/models/message-model.js
+++ src/models/message-model.js
@@ -1,28 +1,66 @@
+/**
+ * @typedef {Object} Message
+ * @property {string} key - The unique key of the message
+ * @property {string} text - The text of the message
+ * @property {string} author - The author of the message
+ * @property {Number} timestamp - The message creation time in ms
+ * @property {string} type - The message type
+ */
+
+/** Class representing a chat message. */
 class Message {
+  /**
+   * Create a message.
+   * @param {Object} data - The x value.
+   */
   constructor(data) {
     this.data = data || {};
   }
 
+  /**
+   * Get the key.
+   * @return {string} The unique key of the message.
+   */
   get key() {
     return this.data.key;
   }
 
+  /**
+   * Get the text.
+   * @return {string} The text of the message.
+   */
   get text() {
     return this.data.title || this.data.message;
   }
 
+  /**
+   * Get the author.
+   * @return {string} The author of the message.
+   */
   get author() {
     return this.data.samurai === undefined ? 'user' : 'bot';
   }
 
+  /**
+   * Get the message creation time.
+   * @return {Number} The message creation time in ms.
+   */
   get timestamp() {
     return this.data.timestamp;
   }
 
+  /**
+   * Get the message type.
+   * @return {string} The message type.
+   */
   get type() {
     return this.data.type;
   }
 
+  /**
+   * Convert this message object class to a simple javascript object.
+   * @return {Message} The message javascript object.
+   */
   toJSON() {
     return {
       key: this.key,
--- src/sections/chat/utils.js
+++ src/sections/chat/utils.js
@@ -1,5 +1,11 @@
 import Message from '../../models/message-model';
 
+/**
+ * parseMessages: Get an object with all the messages.
+ *
+ * @param {Object} messages API Messages model object.
+ * @returns {Object} Object with all the messages using Message model object.
+ */
 export function parseMessages(messages) {
   return Object.values(messages).reduce((obj, next) => {
     obj[next.key] = new Message(next).toJSON();
@@ -7,11 +13,22 @@ export function parseMessages(messages) {
   }, {});
 }
 
+/**
+ * messagesFilter: Determines if it is a basic message type.
+ *
+ * @param {Object} data Message model object.
+ * @returns {Object} True if the message type is basic ('text' or 'dialog').
+ */
 export function messagesFilter(data) {
   /** Support for basic message types */
   return ['text', 'dialog'].includes(data.type);
 }
 
+/**
+ * scrollBottom: Scrolls a DOM element to its bottom position.
+ *
+ * @param {Object} container DOM element.
+ */
 export function scrollBottom(container) {
   if (!container) {
     return;
